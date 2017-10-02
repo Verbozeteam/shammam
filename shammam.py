ff3205449cc1253ae02e359391a15811b950ce2b
Hasan Al-Jawaheri <hasan.aljawaheri@gmail.com>
2017-10-02 21:06:05 +0300

    Made the package callable from outside

--- shammam.py
+++ shammam.py
@@ -14,24 +14,33 @@ for F in cmd_args.source:
     filename = F
     if "/" in F:
         filename = F[F.rindex("/")+1:]
-    os.system("cp {} tmp/{}.cpp".format(F, filename))
+    if os.system("cp {} tmp/{}.cpp".format(F, filename)):
+        print ("File not found: {}".format(filename))
+        exit(1)
 
+INSTALL_DIR = "/Users/hasan/verboze/shammam"
 CPPFLAGS = "-std=c++11"
-BASE_INCLUDE_DIR = "inc"
-BASE_SRC_DIR = "src"
+BASE_INCLUDE_DIR = "{}/inc".format(INSTALL_DIR)
+BASE_SRC_DIR = "{}/src".format(INSTALL_DIR)
 INCLUDE_DIRS = "-I{} -Itmp -Iprotocol".format(BASE_INCLUDE_DIR)
 SOURCE_FILES = "{}/*.cpp tmp/*.cpp tmp/*.cc".format(BASE_SRC_DIR)
 LDFLAGS = "-L/usr/local/lib `pkg-config --libs protobuf grpc++ grpc`"
 OUT_FILE = "shammam"
+PROTOCOL_PATH = "-I{}".format(INSTALL_DIR)
+PROTOCOL_FILENAME = "arduino_protocol.proto".format(INSTALL_DIR)
 
 if cmd_args.include != None:
     INCLUDE_DIRS += " " + " ".join(map(lambda i: "-I{}".format(i), cmd_args.include))
 
-protoc_command = "protoc --cpp_out=tmp --grpc_out=tmp arduino_protocol.proto --plugin=protoc-gen-grpc=$(which grpc_cpp_plugin)"
+protoc_command = "protoc --cpp_out=tmp --grpc_out=tmp {} {} --plugin=protoc-gen-grpc=$(which grpc_cpp_plugin)".format(PROTOCOL_PATH, PROTOCOL_FILENAME)
 gcc_cmd = "g++ {} {} {} -include {}/Arduino.h {} -o {}".format(CPPFLAGS, LDFLAGS, INCLUDE_DIRS, BASE_INCLUDE_DIR, SOURCE_FILES, OUT_FILE)
 
+succeeded = False
 if not os.system(protoc_command): # compile the protocol
     if not os.system(gcc_cmd):
-        os.system("rm -rf tmp/")
-        os.system("./" + OUT_FILE)
+        succeeded = True
+
+os.system("rm -rf tmp/")
+if succeeded:
+    os.system("./" + OUT_FILE)
 
